7fb526b3bb0e70ef3211c12d841c734eb948aab9
Cliff Wu <z800201002005@gmail.com>
2023-05-17 19:18:40 +0900

    Apply Kalman filter for all vehicle attributes except turn signal

--- main.py
+++ main.py
@@ -81,7 +81,15 @@ def main():
             float(data[i][forward_and_rearward_g_idx]))
         lateral_g_measure.append(float(data[i][lateral_g_idx]))
 
+    engine_speed_estimate = []
+    accelerator_opening_angle_estimate = []
+    steering_angle_estimate = []
     speed_estimate = []
+    brake_oil_pressure_estimate = []
+    yaw_rate_estimate = []
+    forward_and_rearward_g_estimate = []
+    lateral_g_estimate = []
+
     # Set Kalman filter parameters
     # consider the current state as the next state (no change)
     state_transition_matrix = 1.0
@@ -91,15 +99,60 @@ def main():
     estimated_error = 0.0  # according to the initialization estimate error
     control_matrix = 0.0  # no control input
     measurable_input = 0.0  # no measurable input
-    kf = KalmanFilterOneDimension(speed_measure[0], state_transition_matrix,
-                                  observation_matrix, process_noise_matrix,
-                                  measurement_noise, estimated_error,
-                                  control_matrix, measurable_input)
-    for i in range(len(speed_measure)):
-        speed_estimate.append(kf.execute(speed_measure[i]))
 
-    with open(OUTPUT_FILE_NAME, 'w') as f:
-        writer = csv.writer(f, lineterminator='\n')
+    # Initialize Kalman filter
+    engine_speed_kf = KalmanFilterOneDimension(engine_speed_measure[0], state_transition_matrix,
+                                               observation_matrix, process_noise_matrix,
+                                               measurement_noise, estimated_error,
+                                               control_matrix, measurable_input)
+    accelerator_opening_angle_kf = KalmanFilterOneDimension(accelerator_opening_angle_measure[0], state_transition_matrix,
+                                                            observation_matrix, process_noise_matrix,
+                                                            measurement_noise, estimated_error,
+                                                            control_matrix, measurable_input)
+    steering_angle_kf = KalmanFilterOneDimension(steering_angle_measure[0], state_transition_matrix,
+                                                 observation_matrix, process_noise_matrix,
+                                                 measurement_noise, estimated_error,
+                                                 control_matrix, measurable_input)
+    speed_kf = KalmanFilterOneDimension(speed_measure[0], state_transition_matrix,
+                                        observation_matrix, process_noise_matrix,
+                                        measurement_noise, estimated_error,
+                                        control_matrix, measurable_input)
+    brake_oil_pressure_kf = KalmanFilterOneDimension(brake_oil_pressure_measure[0], state_transition_matrix,
+                                                     observation_matrix, process_noise_matrix,
+                                                     measurement_noise, estimated_error,
+                                                     control_matrix, measurable_input)
+    yaw_rate_kf = KalmanFilterOneDimension(yaw_rate_measure[0], state_transition_matrix,
+                                           observation_matrix, process_noise_matrix,
+                                           measurement_noise, estimated_error,
+                                           control_matrix, measurable_input)
+    forward_and_rearward_g_kf = KalmanFilterOneDimension(forward_and_rearward_g_measure[0], state_transition_matrix,
+                                                         observation_matrix, process_noise_matrix,
+                                                         measurement_noise, estimated_error,
+                                                         control_matrix, measurable_input)
+    lateral_g_kf = KalmanFilterOneDimension(lateral_g_measure[0], state_transition_matrix,
+                                            observation_matrix, process_noise_matrix,
+                                            measurement_noise, estimated_error,
+                                            control_matrix, measurable_input)
+
+    # Execute Kalman filter
+    for i in range(len(speed_measure)):
+        engine_speed_estimate.append(
+            engine_speed_kf.execute(engine_speed_measure[i]))
+        accelerator_opening_angle_estimate.append(
+            accelerator_opening_angle_kf.execute(accelerator_opening_angle_measure[i]))
+        steering_angle_estimate.append(
+            steering_angle_kf.execute(steering_angle_measure[i]))
+        speed_estimate.append(speed_kf.execute(speed_measure[i]))
+        brake_oil_pressure_estimate.append(
+            brake_oil_pressure_kf.execute(brake_oil_pressure_measure[i]))
+        yaw_rate_estimate.append(yaw_rate_kf.execute(yaw_rate_measure[i]))
+        forward_and_rearward_g_estimate.append(
+            forward_and_rearward_g_kf.execute(forward_and_rearward_g_measure[i]))
+        lateral_g_estimate.append(lateral_g_kf.execute(lateral_g_measure[i]))
+
+    # Output the result
+    with open(OUTPUT_FILE_NAME, "w") as f:
+        writer = csv.writer(f, lineterminator="\n")
         for i in range(COLUMN_NAME_ROW_NUM):
             writer.writerow(data[i])
         writer.writerow([TIME, ENGINE_SPEED, ACCELERATOR_OPENING_ANGLE,
@@ -107,20 +160,61 @@ def main():
                          BRAKE_OIL_PRESSURE, YAW_RATE,
                          FORWARD_AND_REARWARD_G, LATERAL_G])
         for i in range(len(time)):
-            writer.writerow([time[i], engine_speed_measure[i],
-                             accelerator_opening_angle_measure[i],
-                             turn_signal_measure[i], steering_angle_measure[i],
-                             speed_estimate[i], brake_oil_pressure_measure[i],
-                             yaw_rate_measure[i],
-                             forward_and_rearward_g_measure[i],
-                             lateral_g_measure[i]])
+            writer.writerow([time[i], engine_speed_estimate[i], accelerator_opening_angle_estimate[i],
+                             turn_signal_measure[i], steering_angle_estimate[i], speed_estimate[i],
+                             brake_oil_pressure_estimate[i], yaw_rate_estimate[i],
+                             forward_and_rearward_g_estimate[i], lateral_g_estimate[i]])
 
-    plt.title("Speed")
-    plt.plot(speed_measure, label="measurement")
-    plt.plot(speed_estimate, label="estimate")
-    plt.xlabel("Time [s]")
-    plt.ylabel("Speed [km/h]")
-    plt.legend()
+    # Plot the result
+    fig = plt.figure()
+    axes = fig.subplots(nrows=4, ncols=2)
+    fig.suptitle("Kalman filter result")
+    axes[0, 0].plot(time, engine_speed_measure, label="measure")
+    axes[0, 0].plot(time, engine_speed_estimate, label="estimate")
+    axes[0, 0].set_title("Engine speed")
+    axes[0, 0].set_ylabel("[RPM]")
+
+    axes[0, 1].plot(time, accelerator_opening_angle_measure, label="measure")
+    axes[0, 1].plot(time, accelerator_opening_angle_estimate, label="estimate")
+    axes[0, 1].set_title("Accelerator opening angle")
+    axes[0, 1].set_ylabel("[%]")
+
+    axes[1, 0].plot(time, steering_angle_measure, label="measure")
+    axes[1, 0].plot(time, steering_angle_estimate, label="estimate")
+    axes[1, 0].set_title("Steering angle")
+    axes[1, 0].set_ylabel("[°]")
+
+    axes[1, 1].plot(time, speed_measure, label="measure")
+    axes[1, 1].plot(time, speed_estimate, label="estimate")
+    axes[1, 1].set_title("Speed")
+    axes[1, 1].set_ylabel("[km/h]")
+
+    axes[2, 0].plot(time, brake_oil_pressure_measure, label="measure")
+    axes[2, 0].plot(time, brake_oil_pressure_estimate, label="estimate")
+    axes[2, 0].set_title("Brake oil pressure")
+    axes[2, 0].set_ylabel("[MPa]")
+
+    axes[2, 1].plot(time, yaw_rate_measure, label="measure")
+    axes[2, 1].plot(time, yaw_rate_estimate, label="estimate")
+    axes[2, 1].set_title("Yaw rate")
+    axes[2, 1].set_ylabel("[°/s]")
+
+    axes[3, 0].plot(time, forward_and_rearward_g_measure, label="measure")
+    axes[3, 0].plot(time, forward_and_rearward_g_estimate, label="estimate")
+    axes[3, 0].set_title("Forward and rearward G")
+    axes[3, 0].set_ylabel("[m/s^2]")
+
+    axes[3, 1].plot(time, lateral_g_measure, label="measure")
+    axes[3, 1].plot(time, lateral_g_estimate, label="estimate")
+    axes[3, 1].set_title("Lateral G")
+    axes[3, 1].set_ylabel("[m/s^2]")
+
+    fig.tight_layout()
+
+    lines, labels = fig.axes[0].get_legend_handles_labels()
+    fig.legend(lines, labels, loc="upper right")
+
+    # Show the result
     plt.show()
 
 
